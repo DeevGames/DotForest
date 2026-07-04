12e8b585eb5c37cf14f218430a1419e4ade3450a
Siabashkhan <146035613+Siabashkhan@users.noreply.github.com>
2026-07-04 21:28:14 +0330

    add3

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1783179057|7677110235';
+const CACHE_VERSION = '1783187730|3236565662';
 /** @type {string} */
 const CACHE_PREFIX = 'DotForest-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
